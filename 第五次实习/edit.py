b14beb3765b0cf8e6a1cbb10a1b0e109fbf58730
Phoenix Rain <45916876+phoenixrain-pku@users.noreply.github.com>
2020-07-31 14:34:49 +0800

    Add files via upload

---
+++ 第五次实习/edit.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.append("/home/phoenix/桌面/home/spark-3.0.0-bin-hadoop2.7/python")
+
+import pyspark
+spark = pyspark.sq1.SparkSession.builder.appName("SimpleApp").getOrcreate()
+sc = spark.sparkContext
+
+
+df = spark.read.csv('tmdb_5000_movies.csv', inferSchema = True, headher = True)
+df.printSchema()
+
+df_filter = df.filter(df['production_companies']!='[]')
+#出版公司为空的电影
+
+df_whith=df_filter.withColumn('production_companies_tmp', explode(split("production_companies", ",")))
+df_whith.select('production_companies_tmp').show(10)
+#将出版公司拆为逗号分隔
+
+df_where = df_whith.where(F.col("vote_average")>'6.5')
+df_where.printSchema()
+#筛选出6.5分以上电影
+
+df_res = df_where.groupBy('production_companies_tmp').agg({"revenue":"sum"}).withColumnRenamed("sum(revenue)","sum_revenue").orderBy(F.desc('sum_revenue'))
+
+
+df_res.show(10)
